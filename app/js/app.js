23ddca079022ee607185271d3053433d5e4b4aa7
Brendan Goodenough <brendan@goodenough.nz>
2017-12-15 00:01:11 +1300

    Render suggestions in separate section

--- app/js/app.js
+++ app/js/app.js
@@ -5,12 +5,16 @@ const onTextChange = async () => {
   const res = await translate(phrase)
 
   if (!res.length) {
-    return $('#translation').text('')
+    return $('#suggestions').html('Suggestions will be filtered as you type into the English phrase box above.')
   }
 
   const data = JSON.parse(res)
-  const suggestions = data.map(d => `${d.phrase}: ${d.translation}`).join('\n');
-  return $('#translation').text(suggestions)
+  if (!data.length) {
+    return $('#suggestions').html('No suggestions to show')
+  }
+
+  const html = data.map(d => `<li>${d.phrase}: ${d.translation}</li>`).join('');
+  return $('#suggestions').html(html)
 }
 
 $('#phrase').keyup(onTextChange)
